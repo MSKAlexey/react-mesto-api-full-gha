249080ba1cfb96902e03c7ffe3e7e2f18bed8631
Alexey Demin <mskdemin@gmail.com>
2023-07-27 15:03:11 +0300

    add cors

--- backend/app.js
+++ backend/app.js
@@ -8,10 +8,12 @@ const rateLimit = require('express-rate-limit');
 const { errors } = require('celebrate');
 const routes = require('./routes');
 const errorHandler = require('./errors/errors');
+const cors = require('./middlwares/cors');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 app.use(helmet());
+app.use(cors);
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
