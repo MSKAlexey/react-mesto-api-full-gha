8f09bf2092a0a7506889f592c7e7c6c851348ebf
Alexey Demin <mskdemin@gmail.com>
2023-07-31 12:11:47 +0300

    crash

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -19,12 +19,12 @@ export const register = ({ email, password }) => {
 };
 
 export const authorize = ({ email, password }) => {
-  /* const token = localStorage.getItem('jwt'); */
+  const token = localStorage.getItem('jwt');
   return fetch(`${BASE_URL}/signin`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      // 'Authorization': `Bearer ${token}`,
+      'Authorization': `Bearer ${token}`,
     },
     body: JSON.stringify({ email, password }),
   })
