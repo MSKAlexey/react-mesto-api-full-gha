8428c367bc2a56b5855d59b8947b3a7370b49942
Alexey Demin <mskdemin@gmail.com>
2023-08-01 16:31:29 +0300

    email maxLength off

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,54 +3,54 @@ import LoginAndRegistrationWithForm from './LoginAndRegistrationWithForm';
 
 export default function Login({ handelLoginSubmit, /* errorMessage */ }) {
 
- const [formValue, setFormValue] = useState({
-  email: '',
-  password: ''
- });
-
- const { email, password } = formValue;
-
- const handleChange = (e) => {
-  const { name, value } = e.target;
-  setFormValue({
-   ...formValue,
-   [name]: value,
+  const [formValue, setFormValue] = useState({
+    email: '',
+    password: ''
   });
- }
 
- function handleSubmit(e) {
-  e.preventDefault();
-  handelLoginSubmit({ email, password });
- }
+  const { email, password } = formValue;
 
- return (
-  <LoginAndRegistrationWithForm
-   titleText={'Вход'}
-   buttonText={'Войти'}
-   onSubmit={handleSubmit}
-  >
-   <input
-    className='login__input'
-    placeholder='email@mail.com'
-    type='email'
-    name='email'
-    minLength='5'
-    maxLength='15'
-    required
-    onChange={handleChange}
-   />
-   <span id="input-name-error" className="error">{/* errorMessage */}</span>
-   <input
-    className='login__input'
-    placeholder='••••••••••'
-    type='password'
-    name='password'
-    minLength='6'
-    maxLength='15'
-    required
-    onChange={handleChange}
-   />
-   <span id="input-name-error" className="error">{/* errorMessage */}</span>
-  </LoginAndRegistrationWithForm>
- )
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValue({
+      ...formValue,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    handelLoginSubmit({ email, password });
+  }
+
+  return (
+    <LoginAndRegistrationWithForm
+      titleText={'Вход'}
+      buttonText={'Войти'}
+      onSubmit={handleSubmit}
+    >
+      <input
+        className='login__input'
+        placeholder='email@mail.com'
+        type='email'
+        name='email'
+        minLength='5'
+        // maxLength='15'
+        required
+        onChange={handleChange}
+      />
+      <span id="input-name-error" className="error">{/* errorMessage */}</span>
+      <input
+        className='login__input'
+        placeholder='••••••••••'
+        type='password'
+        name='password'
+        minLength='6'
+        maxLength='15'
+        required
+        onChange={handleChange}
+      />
+      <span id="input-name-error" className="error">{/* errorMessage */}</span>
+    </LoginAndRegistrationWithForm>
+  )
 }
