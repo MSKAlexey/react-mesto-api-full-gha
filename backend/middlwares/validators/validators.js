8e67df88e745ffe4b49902ce02b036d65cb843bb
Alexey Demin <mskdemin@gmail.com>
2023-08-01 12:50:09 +0300

    pass min light

--- backend/middlwares/validators/validators.js
+++ backend/middlwares/validators/validators.js
@@ -14,14 +14,14 @@ const validateSingUp = celebrate({
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(regex),
     email: Joi.string().email().required(),
-    password: Joi.string().min().required(),
+    password: Joi.string().required(),
   }),
 });
 
 const validateSingIn = celebrate({
   body: Joi.object().keys({
     email: Joi.string().email().required(),
-    password: Joi.string().min().required(),
+    password: Joi.string().required(),
   }),
 });
 
