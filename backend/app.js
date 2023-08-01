8711df07a57ed4f7af5b4ab1a488d7de7271890a
Alexey Demin <mskdemin@gmail.com>
2023-08-01 14:35:54 +0300

    опять cors(

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,6 @@ const { requestLogger, errorLogger } = require('./middlwares/logger');
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 const app = express();
 app.use(helmet());
-app.use(cors);
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
@@ -35,6 +34,8 @@ const limiter = rateLimit({
 });
 
 app.use(limiter);
+
+app.use(cors);
 
 app.use(routes);
 
--- backend/middlwares/cors.js
+++ backend/middlwares/cors.js
@@ -2,7 +2,7 @@ const allowedCors = [
   'https://alexey.nomoredomains.xyz',
   'https://api.alexey.nomoredomains.xyz',
   'http://localhost:3000',
-  'http://localhost:3001',
+  // 'http://localhost:3001',
 ];
 
 module.exports = (req, res, next) => {
