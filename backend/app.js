b8876bb2560515e3fea05852539d8336d6c68298
Alexey Demin <mskdemin@gmail.com>
2023-09-19 15:28:01 +0300

    3001 port

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,17 @@
 /* eslint-disable import/no-extraneous-dependencies */
-require('dotenv').config();
-const express = require('express');
-const helmet = require('helmet');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const rateLimit = require('express-rate-limit');
-const { errors } = require('celebrate');
-const routes = require('./routes');
-const errorHandler = require('./errors/errors');
-const cors = require('./middlwares/cors');
+require("dotenv").config();
+const express = require("express");
+const helmet = require("helmet");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const rateLimit = require("express-rate-limit");
+const { errors } = require("celebrate");
+const routes = require("./routes");
+const errorHandler = require("./errors/errors");
+const cors = require("./middlwares/cors");
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 3001, DB_URL = "mongodb://127.0.0.1:27017/mestodb" } =
+  process.env;
 const app = express();
 app.use(helmet());
 app.use(cors);
